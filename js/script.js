bd6ccc95f61b737aa15ec12d33fb0bf964cdabbd
Demoniolik <38374157+Demoniolik@users.noreply.github.com>
2019-12-15 14:37:44 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+$(".category .cat").click(function(e){
+  e.preventDefault();
+  $(".category .cat").removeClass("active");
+  $(this).toggleClass("active");
+  itemsSelected();
+});
+
+
+function itemsSelected() {
+
+  if ($(".cat.drinks").hasClass("active")) {
+    $(".items").removeClass("active");
+    $(".items.drinks").toggleClass("active");
+  } else if($(".cat.salad").hasClass("active")) {
+    $(".items").removeClass("active");
+    $(".items.salad").toggleClass("active");
+  } else if($(".cat.firstdish").hasClass("active")) {
+    $(".items").removeClass("active");
+    $(".items.firstdish").toggleClass("active");
+  };
+
+};
+
